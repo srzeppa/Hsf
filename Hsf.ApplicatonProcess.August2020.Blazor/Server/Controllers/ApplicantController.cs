005a2b0a6604dcfb09f8540f0de797f187f698c5
Sławek Rzeppa <fawek@MacBook-Pro-Sawek.local>
2020-08-26 19:12:13 +0200

    Add page confirmation page

--- Hsf.ApplicatonProcess.August2020.Blazor/Server/Controllers/ApplicantController.cs
+++ Hsf.ApplicatonProcess.August2020.Blazor/Server/Controllers/ApplicantController.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Hsf.ApplicatonProcess.August2020.Domain.Models;
 using Hsf.ApplicatonProcess.August2020.Domain.Providers;
 using Hsf.ApplicatonProcess.August2020.Domain.Services;
+using Hsf.ApplicatonProcess.August2020.Domain.Storage;
 using Hsf.ApplicatonProcess.August2020.Domain.Validators;
 using Microsoft.AspNetCore.Mvc;
 
@@ -30,8 +32,26 @@ namespace Hsf.ApplicatonProcess.August2020.Blazor.Server.Controllers
             {
                 return BadRequest(string.Join("\n", result.Errors));
             }
-            await _applicantService.Save(applicant);
+
+            ApplicantStorage.Add(applicant);
+            //await _applicantService.Save(applicant);
             return Ok();
+        }
+
+        [HttpPost]
+        [Route("Confirm-Insert")]
+        public async Task<IActionResult> ConfirmInsert([FromBody] Applicant applicant)
+        {
+            try
+            {
+                await _applicantService.Save(applicant);
+                ApplicantStorage.Remove(applicant);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
         }
 
         [HttpGet]
@@ -61,6 +81,13 @@ namespace Hsf.ApplicatonProcess.August2020.Blazor.Server.Controllers
         {
             await _applicantService.Edit(applicant);
             return Ok();
+        }
+
+        [HttpGet]
+        [Route("GetFromStorage")]
+        public Applicant GetFromStorage(int id)
+        {
+            return ApplicantStorage.Get(id);
         }
     }
 }
